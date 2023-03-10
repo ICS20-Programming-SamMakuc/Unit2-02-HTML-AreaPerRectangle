9ba1dc7b86a08f861eca083cd9910da0caa72ca1
SamMakuc <124847549+SamMakuc@users.noreply.github.com>
2023-03-10 15:56:04 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Created by: Sam Makuc
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('add-math').innerHTML = '3 + 3 + 5 + 5 = ' + (6 + 10) + "cm"
+  document.getElementById('multiply-math').innerHTML = '3 * 5 = ' + (3 * 5) + "cm<sup>2</sup>"
+}
